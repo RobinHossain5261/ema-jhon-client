d55e78b59299bc0b020da726d99f43d223a3cd7d
Robin Hossain <robinhossain5261@gmail.com>
2023-01-05 20:48:00 +0600

    shop & cart done

--- src/Pages/Shared/Cart/Cart.js
+++ src/Pages/Shared/Cart/Cart.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import './Cart.scss';
 
 const Cart = ({ cart }) => {
+
+    // console.log(cart);
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping;
+        quantity = quantity + product.quantity;
+    }
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total + shipping + tax;
+
     return (
-        <div>
-            <h1 className='mt-5 text-center'>Order Summery</h1>
-            <div className='ms-4 mt-5'>
-                <p>Selected Items: {cart.length}</p>
+        <div className='cart ms-4'>
+            <h1 className='mt-5'>Order Summery</h1>
+            <div className=' mt-3'>
+                <p>Selected Items: {quantity}</p>
+                <p>Total Price: ${total}</p>
+                <p>Total Shipping: ${shipping}</p>
+                <p>Tax: ${tax}</p>
+                <h4>Grand Total: ${grandTotal}</h4>
             </div>
         </div>
     );
